80b0f0abbacb0eaa855d79e7047d1dd68d681f4a
sangaryousmane <sangaryousmane@gmail.com>
2024-04-13 22:09:49 +0000

    Encrypt passwprds - Fix pycodestyle

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,7 +8,7 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """Returns an encoded password with salt
     """
-    return  bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
