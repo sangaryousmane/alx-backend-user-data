34aa48581baff31911dfc3a9ff6850f39418d388
sangaryousmane <sangaryousmane@gmail.com>
2024-04-13 22:03:23 +0000

    Encrypt passwprds

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Encrypting passwords
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Returns an encoded password with salt
+    """
+    return  bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Return True if the hashed passowrd matches the original
+    one """
+    encoded = password.encode()
+    return True if bcrypt.checkpw(encoded, hashed_password) else False
