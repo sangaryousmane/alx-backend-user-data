82211c5c35771c6783df868a7d62bca55242835f
sangaryousmane <sangaryousmane@gmail.com>
2024-04-15 21:01:40 +0000

    Implement unauthorized request

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,10 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/api/v1/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized():
+    """ /api/v1/unauthorized
+    """
+    abort(401)
