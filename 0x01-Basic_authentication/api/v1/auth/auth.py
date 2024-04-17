84473f016351fcd649a611c49ccd9787223f8a5b
sangaryousmane <sangaryousmane@gmail.com>
2024-04-17 20:56:05 +0000

    Modify core file to add authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -37,7 +37,9 @@ class Auth:
     def authorization_header(self, request=None) -> str:
         """ Handles authentication header
         """
-        return None
+        if request is None:
+            return None
+        return request.headers.get("Authorization", None)
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Get the current user
