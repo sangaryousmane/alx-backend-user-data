4f79cdd308ff5db5b4574d86cc71b32ca82dc33e
sangaryousmane <sangaryousmane@gmail.com>
2024-04-17 21:00:52 +0000

    Add a Child class to implement Basic authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Basic authentication inheriting from the main Auth class
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ Basic authentication Base class
+    """
+    pass
