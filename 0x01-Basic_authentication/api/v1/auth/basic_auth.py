52ea411aea854420011c3850edc15cfc711015bf
sangaryousmane <sangaryousmane@gmail.com>
2024-04-18 08:31:02 +0000

    . Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,8 @@ Basic authentication inheriting from the main Auth class
 from api.v1.auth.auth import Auth
 import base64
 import binascii
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -50,3 +52,22 @@ class BasicAuth(Auth):
             return (None, None)
         email, password = decoded_base64_authorization_header.split(":", 1)
         return (email, password)
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str
+                                    ) -> TypeVar('User'):
+        """ Get a user object base on the credentials
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        users = User.search(email=user_email)
+        if not users:
+            return None
+         user = users[0]
+
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
+
