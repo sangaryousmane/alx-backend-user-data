3f029943abf77d66a8264cb7da3ff49aa2302cf6
sangaryousmane <sangaryousmane@gmail.com>
2024-04-18 08:03:53 +0000

    Fix basic_auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,4 +8,14 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ Basic authentication Base class
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ Extract base 64 authorization header
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startwith("Basic "):
+            return None
+        return authorization_header[6:]
