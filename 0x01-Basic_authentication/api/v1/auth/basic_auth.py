410d826a0f0974c56976f04307e1114ddfb16c85
sangaryousmane <sangaryousmane@gmail.com>
2024-04-18 08:23:30 +0000

    Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -22,8 +22,9 @@ class BasicAuth(Auth):
             return None
         return authorization_header[6:]
 
-    def decode_base64_authorization_header(self, 
-                                           base64_authorization_header: str) -> str:
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
         """ Decode base64 authorization header
         """
         if base64_authorization_header is None:
@@ -35,3 +36,17 @@ class BasicAuth(Auth):
             return decoded_bytes.decode('utf-8')
         except(TypeError, binascii.Error):
             return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str
+                                 ) -> (str, str):
+        """ Extract the user credentials
+        """
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+        if ":" not in decoded_base64_authorization_header:
+            return (None, None)
+        email, password = decoded_base64_authorization_header.split(":", 1)
+        return (email, password)
