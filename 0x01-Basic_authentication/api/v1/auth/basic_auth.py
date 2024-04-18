e1364758fb5cffe1d131c929677481478d4e8783
sangaryousmane <sangaryousmane@gmail.com>
2024-04-18 08:40:25 +0000

    Basic - User object. Fix bug in the search method

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -54,20 +54,19 @@ class BasicAuth(Auth):
         return (email, password)
 
     def user_object_from_credentials(self, user_email: str, user_pwd: str
-                                    ) -> TypeVar('User'):
+                                     ) -> TypeVar('User'):
         """ Get a user object base on the credentials
         """
         if user_email is None or not isinstance(user_email, str):
             return None
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
-        users = User.search(email=user_email)
+        users = User.search({"email": user_email})
         if not users:
             return None
-         user = users[0]
+        user = users[0]
 
         if not user.is_valid_password(user_pwd):
             return None
 
         return user
-
