c220508b3e6741d0d8e1a35287f21a4d171ce6a8
sangaryousmane <sangaryousmane@gmail.com>
2024-04-18 08:06:40 +0000

    Fix string attribute error

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -16,6 +16,6 @@ class BasicAuth(Auth):
             return None
         if not isinstance(authorization_header, str):
             return None
-        if not authorization_header.startwith("Basic "):
+        if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
